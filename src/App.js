7a7cc1bc80810596899a5ecf6ccb0c8a31570e2c
just-nka <lobzhanidzenikolozi@gmail.com>
2022-02-20 01:22:57 +0400

    changed data

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import Data from "./MOCK_DATA.json";
+import Data from "./data.json";
 import { useState } from "react";
 
 function App() {
@@ -13,26 +13,26 @@ function App() {
           </div>
       </div>
       <br />
-      <input placeholder="find person by name, last name or github username" className='search-bar' onChange={event => setQuery(event.target.value)} />
+      <input placeholder="find person by github username" className='search-bar' onChange={event => setQuery(event.target.value)} />
       {
         Data.filter(person => {
           if (query === '') {
             return person;
-          } else if (person.first_name.toLowerCase().includes(query.toLowerCase()) || person.last_name.toLowerCase().includes(query.toLowerCase()) || person.github_username.toLowerCase().includes(query.toLowerCase())) {
+          } else if (person.login.toLowerCase().includes(query.toLowerCase())) {
             return person;
           }
         }).map((person, index) => (
           <div className="overlay-box" key={index}>
             <div className="box">
-              <a href={`https://github.com/@${person.github_username}`} className="github-link" target="_blank">
+              <a href={person.html_url} className="github-link" target="_blank">
                 <div className="person-profile">
-                  <img src={person.profile_picture} className="profile-picture" alt="" /> 
-                  <p className='person-name'> {person.first_name} {person.last_name}</p>
+                  <img src={person.avatar_url} className="profile-picture" alt="" /> 
+                  <p className='person-name'> {person.login} </p>
                 </div>
                 <div className="person-info">
-                  <p>ელ. ფოსტა: <a href={`mailto:${person.email}`}>{person.email}</a></p>
-                  <p>სქესი: <b>{person.gender}</b></p>
-                  <a href={`https://github.com/@${person.github_username}`} className="person-github-link" target="_blank"><button className='person-github'><b>github-ზე გადასვლა</b></button></a>
+                  <p>ID: <b>{person.id}</b></p>
+                  <p>User Type: <b>{person.type}</b></p>
+                  <a href={person.html_url} className="person-github-link" target="_blank"><button className='person-github'><b>github-ზე გადასვლა</b></button></a>
                 </div>
               </a>
             </div>
